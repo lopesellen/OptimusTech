824b82d928054c39ddb7138cc357a89e635e738b
redfieldellen <105083052+lopesellen@users.noreply.github.com>
2022-05-10 13:07:39 -0300

    index.js removed
    arquivo está na pasta js.

--- index.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('.menu')
-const menuIcon = document.querySelector('.sideimg')
-
-menuIcon.addEventListener('click', function() {
-  // quando houver clique no ícone de menu
-  menu.classList.toggle('active')
-})
-
